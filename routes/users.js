ed43f7f6a9a9dee65f699fd354c91de1ffede0bb
Abhishek Ramesh <abhiisheek@hotmail.com>
2024-04-18 18:56:17 +0530

    (add): Signup and login user api

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,46 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
 
 const User = require("../models/user");
 
-router.post("/login", function (req, res, next) {
+router.post("/signup", async (req, res, next) => {
   const email = req.body.email;
-  const password = req.params.password;
+  const name = req.body.name;
+  const password = req.body.password;
 
   User.find({ email })
+    .exec()
+    .then(async (docs) => {
+      if (docs?.length) {
+        res.status(400).send("User already exists");
+      } else {
+        const newUser = new User({
+          _id: new mongoose.Types.ObjectId(),
+          name,
+          email,
+          password,
+        });
+
+        await newUser.save();
+
+        res.send(newUser);
+      }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.find({ email, password })
     .exec()
     .then((docs) => res.send(docs));
 });
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   User.find()
     .select("name email")
     .exec()
